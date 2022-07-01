74c45f5ebd36d2880b6438cc07a892daf1e26411
Chayan Das <chayandas0308@gmail.com>
2022-07-01 11:38:07 +0530

    day5 changes

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,58 +1,57 @@
 import 'package:flutter/material.dart';
 
+import '../utils/routes.dart';
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_image.png",
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const Text("Welcome",
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical:16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-              decoration: const InputDecoration(
-                hintText: "Enter Username",
-                labelText: "Username"
-              ),
-            ),
-
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: "Enter Password",
-                labelText: "Password"
-              ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
             ),
             const SizedBox(
               height: 20.0,
             ),
-            ElevatedButton(child: Text("Login"),
-            style: TextButton.styleFrom(),
-            onPressed: (){
-              print("hi world");
-            },
-                        
-            )
-              ],
+            const Text("Welcome",
+                style:
+                    const TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            const SizedBox(
+              height: 20.0,
             ),
-          )
-
-        ],
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  const SizedBox(
+                    height: 40.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
 
       // child: Center(
