43a30e09cf57e0446faaff6af8a02ee193898f4a
Chayan Das <chayandas0308@gmail.com>
2022-07-01 10:53:18 +0530

    till day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Container(
-        child: Text("Welcome to new app"),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: GoogleFonts.lato().fontFamily
       ),
+      // home: HomePage(),
+      // initialRoute: "/home",
+      routes: {
+        "/": (context)=> LoginPage(),
+        "/home": (context)=> HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
+
